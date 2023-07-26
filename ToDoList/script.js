c24487ed28f00a1df9deecf076492bcafa7b41f5
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-07-26 16:47:35 -0300

    finaliza to-do list

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -13,4 +13,29 @@ function addTask() {
     li.appendChild(span);
   }
   inputBox.value = "";
+  saveData();
 }
+
+listContainer.addEventListener(
+  // Marcar como feito ou excluir
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  // Salvar as tarefas
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask()
