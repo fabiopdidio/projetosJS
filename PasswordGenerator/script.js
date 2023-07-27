9c2832fbd45d561875573bd4946e44532f2d46c6
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-07-27 17:11:13 -0300

    finaliza

---
+++ PasswordGenerator/script.js
@@ -0,0 +1,28 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%¨&*()_+-{}[]></=";
+
+const alChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (length > password.length) {
+    //continua adicionando até chegar a 12 caracteres
+    password += alChars[Math.floor(Math.random() * alChars.length)];
+  }
+  passwordBox.value = password;
+}
+
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
+}
