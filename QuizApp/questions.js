b5b65644c67eddbd67b55fa2703c62666a706ca6
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-07-31 16:16:52 -0300

    add export import

---
+++ QuizApp/questions.js
@@ -0,0 +1,29 @@
+export const questions = [
+  {
+    question: "Which is the largest animal in the world?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue Whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    question: "Which is the smallest country in the world?",
+    answers: [
+      { text: "Vatican City", correct: true },
+      { text: "Bhutan", correct: false },
+      { text: "Nepal", correct: false },
+      { text: "Macau", correct: false },
+    ],
+  },
+  {
+    question: "Which is the largest country in the world?",
+    answers: [
+      { text: "Russia", correct: true },
+      { text: "China", correct: false },
+      { text: "USA", correct: false },
+      { text: "Brazil", correct: false },
+    ],
+  },
+];
--- QuizApp/script.js
+++ QuizApp/script.js
@@ -1,32 +1,4 @@
-const questions = [
-  {
-    question: "Which is the largest animal in the world?",
-    answers: [
-      { text: "Shark", correct: false },
-      { text: "Blue Whale", correct: true },
-      { text: "Elephant", correct: false },
-      { text: "Giraffe", correct: false },
-    ],
-  },
-  {
-    question: "Which is the smallest country in the world?",
-    answers: [
-      { text: "Vatican City", correct: true },
-      { text: "Bhutan", correct: false },
-      { text: "Nepal", correct: false },
-      { text: "Macau", correct: false },
-    ],
-  },
-  {
-    question: "Which is the largest country in the world?",
-    answers: [
-      { text: "Russia", correct: true },
-      { text: "China", correct: false },
-      { text: "USA", correct: false },
-      { text: "Brazil", correct: false },
-    ],
-  },
-];
+import { questions } from "./questions.js";
 
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
