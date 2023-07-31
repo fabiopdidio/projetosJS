7177cfca3ed505e1dafd4d9ef9ce09bb1d1f55e9
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-07-31 11:10:57 -0300

    add js

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -1,1 +1,11 @@
-const question = document.getElementById("question")
+const questions = [
+  {
+    question: "Which is the largest animal in the world?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue Whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+];
