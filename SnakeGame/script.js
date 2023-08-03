e2fe3f9184a79fb31a9f9824ba4a3af05245d640
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-08-03 16:52:11 -0300

    update js

--- SnakeGame/script.js
+++ SnakeGame/script.js
@@ -1,1 +1,21 @@
-const playBoard = document.querySelector(".play-board")
+const playBoard = document.querySelector(".play-board");
+const scoreElement = document.querySelector("score");
+const highScoreElement = document.querySelector("high-score");
+const controls = document.querySelector("controls i");
+
+let gameOver = false;
+let foodX, foodY;
+let snakeX = 5,
+  snakeY = 5;
+let velocityX = 0,
+  velocityY = 0;
+let snakeBody = [];
+let setIntervalld;
+let score = 0;
+let highScore = localStorage.getItem("hish-score") || 0;
+highScoreElement.innerText = `High score: ${highScore}`;
+
+const updateFoodPosition = () => {
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
+};
